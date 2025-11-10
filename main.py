07cf7663fb69b7645d8de449f71fe68de443c7ef
danigoldblat <d0504109212@gmail.com>
2025-11-10 15:18:24 +0200

    main_book+main_user

--- main.py
+++ main.py
@@ -9,3 +9,28 @@ library.add_book(book)
 library.add_user(user)
 print(library)
 print(library.borrow_book(77896,554))
+
+
+
+
+choice = None
+while choice != "7":
+    print("1. Add Book\n2. Add User\n3. Borrow Book\n7. Save & Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        titel = input("Enter the book name")
+        author = input("Enter the author's name")
+        ISBN = input("Enter the book's ID number.")
+        book = Book(titel,author,ISBN)
+        library.add_book(book)
+    
+    elif choice == "2":
+        naim = input("Enter your name")
+        id = input("Enter your identification number")
+        user = User(naim,id)        
+        library.add_user(user)
+    elif choice == "7":
+# save data and exit
+            break
+    else:
+        print("Invalid choice, try again.")
